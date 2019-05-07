2335928fcdaf5ea5264c6241a9306cddeb0ed256
hwy02 <1025097410@qq.com>
2019-05-07 14:23:43 +0800

    first commit

---
+++ BXS_sdam/BXS_sdam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BXS_sdam
+//
+//  Created by 黄伟业 on 2019/5/7.
+//  Copyright © 2019 黄伟业. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BXS_sdam/BXS_sdamTests/BXS_sdamTests.m
@@ -0,0 +1,37 @@
+//
+//  BXS_sdamTests.m
+//  BXS_sdamTests
+//
+//  Created by 黄伟业 on 2019/5/7.
+//  Copyright © 2019 黄伟业. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BXS_sdamTests : XCTestCase
+
+@end
+
+@implementation BXS_sdamTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
